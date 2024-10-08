f842d7b3e0d8bf0850a456e21059350b1f9d2fd6
gurevdm04 <gurevdm04@gmail.com>
2024-10-08 21:54:18 +1000

    -

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.patch(
 app.get("/comments/:postId", CommentController.getCommentsByPost);
 app.post("/comments", checkAuth, CommentController.addComment);
 
-const port = process.env.PORT || 9001;
+const port = 3000;
 app.listen(port, (err) => {
   if (err) {
     return console.log(err);
